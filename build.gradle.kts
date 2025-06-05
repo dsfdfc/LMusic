0fa087b6489df459f06e26151d26aa6e01874033
dsfdfc <86122232+dsfdfc@users.noreply.github.com>
2025-06-05 10:06:53 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,31 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.application) apply false
-    alias(libs.plugins.library) apply false
-    alias(libs.plugins.kotlin) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.flyjingfish.aop) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.krouter.plugin)
-    alias(libs.plugins.lumo)
-}
+dependencies {
+    val room_version = "2.7.1"
 
-// 配置注入遍历的起点项目
-ext { set("targetInjectProjectName", "app") }
+    implementation("androidx.room:room-runtime:$room_version")
+
+    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
+    // See Add the KSP plugin to your project
+    ksp("androidx.room:room-compiler:$room_version")
+
+    // If this project only uses Java source, use the Java annotationProcessor
+    // No additional plugins are necessary
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
+}
